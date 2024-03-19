460de31e74123e4ff0d395a37802dfa827f5a6fa
Josh <80289665+DiamondsCode@users.noreply.github.com>
2024-03-19 12:18:44 -0700

    gen.php - PUT ON WEBSERVER

---
+++ gen.php
@@ -0,0 +1,24 @@
+<?php
+
+function generateRandomID($length = 3) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+function generateRandomID2($length = 6) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $randomString;
+}
+
+$id = generateRandomID();
+$id2 = generateRandomID2();
+echo $id . '=-' . $id2;
+
+?>
